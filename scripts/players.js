cc999f649f3df329ccd6814614004863265d2c45
vondalisay <yvonnedalisay@gmail.com>
2021-07-28 20:55:58 +0800

    temporarily remove players not yet started in axie

--- scripts/players.js
+++ scripts/players.js
@@ -16,30 +16,31 @@ var playersList =
     id: "0xe93f2622be726d62c371a0589a47d2da8c683f9ew",
     name: "Jiz",
     status: "probationary",
-    team: ["plant", "beast", "beast"]
-  },
-  {
-    id: "0xe93f2622be726d62c371a0589a47d2da8c683f9dg",
-    name: "Jabster",
-    status: "probationary",
     team: ["plant", "aqua", "beast"]
   },
-  {
-    id: "0xe93f2622be726d62c371a0589a47d2da8c683f9dh",
-    name: "Jen",
-    status: "probationary",
-    team: ["plant", "aqua", "beast"]
-  },
-  {
-    id: "0xe93f2622be726d62c371a0589a47d2da8c683f9dm",
-    name: "JunEva",
-    status: "probationary",
-    team: ["plant", "aqua", "aqua"]
-  },
-  {
-    id: "0xe93f2622be726d62c371a0589a47d2da8c683f9dj",
-    name: "Monally",
-    status: "probationary",
-    team: ["plant", "aqua", "aqua"]
-  }
+  // {
+  //   id: "0xe93f2622be726d62c371a0589a47d2da8c683f9dg",
+  //   name: "Augustusaurus",
+  //   status: "probationary",
+  //   team: ["plant", "aqua", "beast"]
+  
+  // },
+  // {
+  //   id: "0xe93f2622be726d62c371a0589a47d2da8c683f9dh",
+  //   name: "Jen",
+  //   status: "probationary",
+  //   team: ["plant", "aqua", "beast"]
+  // },
+  // {
+  //   id: "0xe93f2622be726d62c371a0589a47d2da8c683f9dm",
+  //   name: "JunEva",
+  //   status: "probationary",
+  //   team: ["plant", "aqua", "aqua"]
+  // },
+  // {
+  //   id: "0xe93f2622be726d62c371a0589a47d2da8c683f9dj",
+  //   name: "Monally",
+  //   status: "probationary",
+  //   team: ["plant", "aqua", "aqua"]
+  // }
 ]
