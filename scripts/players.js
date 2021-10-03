4624662ab2de521aa06ac2ab628a3a521da3c14b
yatcdeaxie <vonaceaxie@gmail.com>
2021-10-04 00:22:07 +0800

    update team compo of jence and alliepot

--- scripts/players.js
+++ scripts/players.js
@@ -4,7 +4,6 @@ var playersList =
     id: "0xe93f2622be726d62c371a0589a47d2da8c683f9d",
     name: "Jaycee",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -13,7 +12,6 @@ var playersList =
     id: "0x00f1224b055edcb0accab42eda55df0bf00f48a4",
     name: "Big Boss III",
     team: ["plant", "reptile", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -22,7 +20,6 @@ var playersList =
     id: "0xbd2cd6f79113c2780e0ec353fc49d16aadc94d9e",
     name: "Trololol",
     team: ["plant", "reptile", "beast"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     deductedSlp: 0,
@@ -31,8 +28,7 @@ var playersList =
   {
     id: "0x472a0453d9223768053e410bb30d19d5122d1598",
     name: "Jence",
-    team: ["plant", "aqua", "beast"],
-    manager: "YA",
+    team: ["plant", "dusk", "beast"],
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -41,7 +37,6 @@ var playersList =
     id: "0x984aa6669a897997770ca21ea46f59bb409067b2",
     name: "Augustusaurus",
     team: ["plant", "reptile", "beast"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -49,8 +44,7 @@ var playersList =
   {
     id: "0x4d7c90157257839004927540bad56d479e93273d",
     name: "Alliepot",
-    team: ["plant", "aqua", "beast"],
-    manager: "YA",
+    team: ["plant", "reptile", "beast"],
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -59,7 +53,6 @@ var playersList =
     id: "0xadc3db0cf7b76955e707f99e2c1760139ebc8d1f",
     name: "TakQuiz",
     team: ["plant", "plant", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -68,7 +61,6 @@ var playersList =
     id: "0x65d970953bbe77654553a7069c5a18fe7aaa901d",
     name: "Carl19",
     team: ["plant", "reptile", "beast"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -77,7 +69,6 @@ var playersList =
     id: "0xef453a4ce4eb92fdfcc97a097586c50446e9ec03",
     name: "Eiram",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -86,7 +77,6 @@ var playersList =
     id: "0x00a1e039c77a8a5f51e47567e59c40780d7a8c47",
     name: "Monay",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -95,7 +85,6 @@ var playersList =
     id: "0x9e441983480745a4d4bf4499e0a84f01648104e5",
     name: "Mellow Whiskey",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -104,7 +93,6 @@ var playersList =
     id: "0xbe80e12b74bf3de5a4474a55c53f6870ac41dfb6",
     name: "Okpy",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -113,7 +101,6 @@ var playersList =
     id: "0xe59ebe504549c9bdf7c47691f0dcb64f8f25347a",
     name: "Chicklet",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -122,7 +109,6 @@ var playersList =
     id: "0x4eef0012bfad763050b3155366de3c38e02ea630",
     name: "Melvin",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 1305,
     unclaimedSlp: 457,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -131,7 +117,6 @@ var playersList =
     id: "0xf4f0696ef63f777a599633e310c84d9d3042ec9d",
     name: "Thirds",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 1294,
     unclaimedSlp: 518,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -140,7 +125,6 @@ var playersList =
     id: "0x9c30fc866ea789a53899b6478b50d6f22ceb3e99",
     name: "Meys",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 711,
     unclaimedSlp: 213,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -149,7 +133,6 @@ var playersList =
     id: "0xd42f848361be0fcdd5a73fcc2858c7f07a213c6e",
     name: "Kie",
     team: ["plant", "aqua", "aqua"],
-    manager: "YA",
     totalFarmedPrevCutoff: 156,
     unclaimedSlp: 47,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -158,7 +141,6 @@ var playersList =
     id: "0x8a17825f5e3414815b4398afa999a3ef552649da",
     name: "SoloGamer003",
     team: ["plant", "dusk", "mech"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 3 2021 08:00:00 GMT+0800"
@@ -167,7 +149,6 @@ var playersList =
     id: "0x70bcb78b4afd98201984bc01c364290c55acf88e",
     name: "Mikoy",
     team: ["plant", "dusk", "beast"],
-    manager: "YA",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 3 2021 08:00:00 GMT+0800"
@@ -176,7 +157,6 @@ var playersList =
     id: "0xfadb0ee2909568f0fef334833745463b62e1656a",
     name: "Abateka",
     team: ["plant", "aqua", "mech"],
-    manager: "YA",
     totalFarmedPrevCutoff: 77,
     unclaimedSlp: 0,
     startDate: "Oct 3 2021 08:00:00 GMT+0800"
@@ -185,7 +165,6 @@ var playersList =
     id: "0xca44ecc383572215e47d4eda640183cab6816c47",
     name: "Jeffrey Valerio",
     team: ["plant", "reptile", "mech"],
-    manager: "VD",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -194,7 +173,6 @@ var playersList =
     id: "0x8ec62ab60f293c9eba1d319874d5483886ea49be",
     name: "Berna",
     team: ["plant", "reptile", "beast"],
-    manager: "VD",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -203,7 +181,6 @@ var playersList =
     id: "0x715b802842b6f5fea16496afeceb592af123712b",
     name: "Artemiz",
     team: ["plant", "reptile", "beast"],
-    manager: "VD",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 6 2021 08:00:00 GMT+0800"
@@ -212,7 +189,6 @@ var playersList =
     id: "0x733677045cae9c8098149697d8f72b7e6c7ef157",
     name: "Radyork",
     team: ["plant", "reptile", "mech"],
-    manager: "KD",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 1 2021 08:00:00 GMT+0800"
@@ -221,7 +197,6 @@ var playersList =
     id: "0xdb8e152e80f50c59695265b939f4d7f5911b789e",
     name: "Dlwlrma",
     team: ["plant", "reptile", "beast"],
-    manager: "KD",
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "Oct 6 2021 08:00:00 GMT+0800"
