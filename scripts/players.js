b67b45df982bfae92217dac17853377cbcacf809
yatcdeaxie <vonaceaxie@gmail.com>
2021-11-24 17:23:24 +0800

    add scholar nam shin

--- scripts/players.js
+++ scripts/players.js
@@ -248,5 +248,13 @@ var playersList =
     totalFarmedPrevCutoff: 528,
     unclaimedSlp: 133,
     startDate: "November 16 2021 08:00:00 GMT+0800"
+  },
+  {
+    id: "0xb2ede112a85c001e53587067867762ecfe649b1d",
+    name: "Nam Shin",
+    team: ["plant", "reptile", "mech"],
+    totalFarmedPrevCutoff: 0,
+    unclaimedSlp: 0,
+    startDate: "November 24 2021 08:00:00 GMT+0800"
   }
 ]
