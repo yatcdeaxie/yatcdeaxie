6495a301db6dde6a73358bc20d934e1449560af3
yatcdeaxie <vonaceaxie@gmail.com>
2022-02-27 21:15:58 +0800

    remove jence added pogsgaming

--- scripts/players.js
+++ scripts/players.js
@@ -27,8 +27,8 @@ var playersList =
   },
   {
     id: "0x472a0453d9223768053e410bb30d19d5122d1598",
-    name: "Jence",
-    team: ["plant", "dusk", "beast"],
+    name:"PogsGaming",
+    team: ["plant", "reptile", "beast"],
     totalFarmedPrevCutoff: 0,
     unclaimedSlp: 0,
     startDate: "February 27 2022 08:00:00 GMT+0800"
